de9b34e3e74b6629602fae37a4fce7e8e2f7d481
ChengTsungPao <abaozheng6@gmail.com>
2019-02-20 17:34:52 +0800

    test

---
+++ Hello.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout<<"Hi";
+    system("pause");
+    return 0;
+}
---
+++ test.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("Hello_everyone\n");
+    system("pause");
+    return 0;
+}
