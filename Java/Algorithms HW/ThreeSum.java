952761497056ac1844cf6305972b16654170df63
ChengTsungPao <abaozheng6@gmail.com>
2019-03-13 01:22:05 +0800

    learning java

---
+++ Java/Algorithms HW/ThreeSum.java
@@ -0,0 +1,3 @@
+public abstract class ThreeSum {
+	public abstract int T_sum(int[] A);
+}
---
+++ Java/Algorithms HW/ThreeSumMain.java
@@ -0,0 +1,10 @@
+class ThreeSumMain{
+    public static void main(String[] args) {
+
+        ThreeSum4105054025 test = new ThreeSum4105054025();
+        int[] a = {1,2,3,-1,-2,-1};  //{-2,-1,-1,1,2,3}
+        System.out.println(test.T_sum(a));
+
+    }
+
+}
