23edd80de0f28d4635c65e87fda47b53466cf2a7
ChengTsungPao <abaozheng6@gmail.com>
2019-04-06 15:36:33 +0800

    learning python

--- python/HW2_1.py
+++ python/HW2_1.py
@@ -1,88 +1,48 @@
-
-# coding: utf-8 
-
-# ### Layers : Fundamental blocks of Neural Network
-
-# In[1]:
-
-
 import torch
 from torch.nn import Linear, ReLU
 import torch.nn as nn
 import numpy as np
 from torch.autograd import Variable
 
-
-# In[2]:
-
-
 myLayer = Linear(in_features=10,out_features=5,bias=True)
 inp = Variable(torch.randn(1,10))
 myLayer = Linear(in_features=10,out_features=5,bias=True) 
 myLayer(inp)
 
-# In[3]:
-
-
 myLayer.weight
-
-
-# In[4]:
-
-
 myLayer.bias
-
-
-# ### Stacking Linear layers
-
-# In[5]:
-
 
 myLayer1 = Linear(10,5)
 myLayer2 = Linear(5,2)
 myLayer2(myLayer1(inp))
 
-
-# ### PyTorch Non-linear Activations
-
-# In[6]:
-
-
 sample_data = Variable(torch.Tensor([[1,2,-1,-1]])) 
 myRelu = ReLU()
 myRelu(sample_data)
  
-
-# In[7]:
-
-
 import torch.nn as nn
 import torch.nn.functional as F
 sample_data = Variable(torch.Tensor([[1,2,-1,-1]])) 
 f = F.relu(sample_data) # Much simpler.
 f
 
-
-# ### Neural Network 
-
-# In[8]:
-
-
 class MyFirstNetwork(nn.Module):
     def __init__(self,input_size,hidden_size,output_size):
         super(MyFirstNetwork,self).__init__() 
         self.layer1 = nn.Linear(input_size,hidden_size) 
         self.layer2 = nn.Linear(hidden_size,output_size)
-    def __forward__(self,input): 
+    def __getitem__(self):
+        first  = [self.layer1.weight,self.layer1.bias]
+        second = [self.layer2.weight,self.layer2.bias]
+        return first,second
+    def forward(self,input): 
         out = self.layer1(input) 
-        out = nn.ReLU(out)
+        out = F.relu(out)
         out = self.layer2(out) 
         return out
 
-
-# ### Loss
-
-# In[9]:
+model = MyFirstNetwork(224*224,56*56,2)
+print(model)
 
 
 loss = nn.MSELoss()
@@ -91,28 +51,30 @@ target = Variable(torch.randn(3, 5))
 output = loss(input, target)
 output.backward()
 
-
-# In[10]:
-
-
 def cross_entropy(true_label, prediction):
     if true_label == 1:
         return -log(prediction)
     else:
         return -log(1 - prediction)
 
-
-# In[11]:
-
-
 loss = nn.CrossEntropyLoss()
 input = Variable(torch.randn(3, 5), requires_grad=True) 
 target = Variable(torch.LongTensor(3).random_(5)) 
 output = loss(input, target)
-#print(input)
-#print(target)
-#print(output)
 output.backward()
+print(output.data.item())
+
+def CrossEntropyLoss(input,target):
+    sum = 0
+    for i in range(len(input)):
+        tmp = 0
+        for j in range(len(input[0])):
+            tmp+=np.e**(input[i][j])
+        sum+=-input[i][target[i]]+np.log(tmp)
+    return float(sum)/len(input)
+
+print(CrossEntropyLoss(input.data.numpy(),target.data.numpy()))
+
 
 
 from glob import glob
@@ -134,13 +96,7 @@ class DogsAndCatsDataset(Dataset):
         return input,target
 
 dataset=DogsAndCatsDataset("D:/program/vscode_workspace/private/data/dogs-vs-cats/sample/*.jpg")
-#import matplotlib.pylab as plt
-#plt.imshow(dataset[0][0])
-#plt.show()
 
-# In[14]:
-
-# for demo
 import torch.optim as optim
 loss_fn = nn.MSELoss()
 model1 = nn.Linear(224*224,56*56)
@@ -152,13 +108,14 @@ for input, target in dataset:
     #print(input.float().type())
     #print (model.weight.type())
     data=input[:,:,0].view(1,-1).float()+input[:,:,1].view(1,-1).float()+input[:,:,2].view(1,-1).float()
+    print(model(data/(3*(255.)**2)**0.5))
     output = model1(data/(3*(255.)**2)**0.5)
     output = myRelu(output)
     output = model2(output)
     output = myRelu(output)
     output = model3(output)    
     output = F.softmax(output,dim=1)
-    print(output)
+    print(output)    
     optimizer.zero_grad()
     loss = loss_fn(output, target.float())
     loss.backward()
