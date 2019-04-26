b96dbca83c6dc6419c2d12a8f20cbb1070c597aa
ChengTsungPao <abaozheng6@gmail.com>
2019-04-26 19:07:13 +0800

    learning java

--- Java/Algorithms HW/HW6/SortingArrayMain.java
+++ Java/Algorithms HW/HW6/SortingArrayMain.java
@@ -1,8 +1,9 @@
 class SortingArrayMain{
     public static void main(String[] args) {
-        int[] A = {1,2,5,7,4,2,1,7,1,2,5,7,4,2,1};
-        SortingArray41050540252 test = new SortingArray41050540252();
+        int[] A = {1,2,5,7,4,2,1,7,1,2,5,7,4,2,1,5,4,8,10,11,6,47,7,0,5,7,9,100,5,8,9,1,12,11,7,8-1,-5};
+        SortingArray41050540253 test = new SortingArray41050540253();
         show(test.sorting(A));  
+        System.out.println(A.length);
     }
     static void show(int[] A) {
         for(int i=0;i<A.length;i++)
