c40281481abe18290980ab684b2d59dfac218eb0
ChengTsungPao <abaozheng6@gmail.com>
2019-05-19 01:03:17 +0800

    learning java

---
+++ Java/Algorithms HW/HW7/LSPath41050540252.java
@@ -0,0 +1,10 @@
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class LSPath41050540252 extends LSPath{
+    public int Ans(int[][] list){
+
+        return 1;
+    }
+}
--- Java/Algorithms HW/HW7/LSPathMain.java
+++ Java/Algorithms HW/HW7/LSPathMain.java
@@ -1,7 +1,7 @@
 class LSPathMain{
     public static void main(String[] args) {
         int[][] G = {{0,1},{0,2},{0,4},{1,3},{1,4},{2,5},{6,7}};
-        LSPath4105054025 test = new LSPath4105054025();
+        LSPath41050540252 test = new LSPath41050540252();
         System.out.println(test.Ans(G));        
     }
 }
