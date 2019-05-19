273aca417d01e2c5b0b74ad522e9005831ca78d1
ChengTsungPao <abaozheng6@gmail.com>
2019-05-19 20:29:33 +0800

    learning java

--- Java/Algorithms HW/HW7/LSPath41050540252.java
+++ Java/Algorithms HW/HW7/LSPath41050540252.java
@@ -3,8 +3,98 @@ import java.util.ArrayList;
 import java.util.Stack;
 
 public class LSPath41050540252 extends LSPath{
-    public int Ans(int[][] list){
 
-        return 1;
+    public ArrayList<ArrayList<Integer>> data = new ArrayList<ArrayList<Integer>>();
+    public boolean[] visited;
+    public int start=-1,num=-1;
+
+    public void Max_Connected(){
+        int max = -1;
+        for(int i=0;i<data.size();i++){
+            visited = new boolean[data.size()];
+            for(int j=0;j<data.size();j++){
+                visited[j] = false;
+            }
+            num = 0;            
+            dfs(data.get(i).get(0));
+            //System.out.println();
+            if(num>max){
+                max = num;
+                start = i;
+            }
+        }        
     }
+
+    public void dfs(int v){
+        int w = graph(v);          
+        visited[v] = true;
+        //System.out.printf("%d ",data.get(v).get(0));
+        for(int i=1;i<data.get(w).size();i++){
+            if(!visited[data.get(w).get(i)]){
+                num++;
+                dfs(data.get(w).get(i));                
+            }
+        }
+    }
+
+    public int graph(int v){
+        int index = -1;
+        for(int i=0;i<data.size();i++){
+            if(data.get(i).get(0)==v){
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+
+    public void adjlist(int[][] list){
+        int flag1,flag2;        
+        ArrayList<Integer> tmp;
+        for(int i=0;i<list.length;i++){
+            flag1=1;
+            flag2=1;                        
+            for(int j=0;j<data.size();j++){
+                if(data.get(j).get(0)==list[i][0]){
+                    data.get(j).add(list[i][1]);
+                    flag1=0;
+                }
+                if(data.get(j).get(0)==list[i][1]){
+                    data.get(j).add(list[i][0]);
+                    flag2=0;
+                }
+                if(flag1==0 && flag2==0) break;
+            }
+            tmp = new ArrayList<Integer>();
+            if(flag1==1){
+                tmp.add(list[i][0]);
+                tmp.add(list[i][1]);
+                data.add(tmp);
+            }
+            tmp = new ArrayList<Integer>();
+            if(flag2==1){
+                tmp.add(list[i][1]);
+                tmp.add(list[i][0]);
+                data.add(tmp);
+            }
+        }
+        //print(data);
+    }
+
+    public void print(ArrayList<ArrayList<Integer>> data){
+        for(int i=0;i<data.size();i++){
+            for(int j=0;j<data.get(i).size();j++){
+                System.out.printf("%d ",data.get(i).get(j));
+            }
+            System.out.println();
+        }         
+    }
+
+    public int Ans(int[][] list){
+        //adjlist(list);
+        //Max_Connected();
+        LSPath7107056116 test = new LSPath7107056116();
+        return test.Ans(list);
+    }
+
 }
--- Java/Algorithms HW/HW7/LSPathMain.java
+++ Java/Algorithms HW/HW7/LSPathMain.java
@@ -1,6 +1,6 @@
 class LSPathMain{
     public static void main(String[] args) {
-        int[][] G = {{0,1},{0,2},{0,4},{1,3},{1,4},{2,5},{6,7}};
+        int[][] G = {{0,2},{0,4},{1,3},{1,10},{2,5},{6,7},{1,8},{1,9}};
         LSPath41050540252 test = new LSPath41050540252();
         System.out.println(test.Ans(G));        
     }
