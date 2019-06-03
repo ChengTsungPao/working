50331ff445d8c61256fc5e9542a78a9802b8c3f4
ChengTsungPao <abaozheng6@gmail.com>
2019-06-03 17:54:01 +0800

    learning java

--- Java/Algorithms HW/HW8/ContinuousMain.java
+++ Java/Algorithms HW/HW8/ContinuousMain.java
@@ -1,7 +1,7 @@
 class ContinuousMain{
     public static void main(String[] args) {
         int[] A = {1,2,6,-5,2,4};
-        Continuous4105054025 test = new Continuous4105054025();
+        Continuous41050540252 test = new Continuous41050540252();
         System.out.println(test.min_sum(A));
     }
 
