4775102928a94cfb639fda0c4e4065aa11354aa8
ChengTsungPao <abaozheng6@gmail.com>
2019-08-08 22:19:42 +0800

    learning python

---
+++ python/Pytorch/plot.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+file = np.load("D:/program/vscode_workspace/private/data/project_train/npzfile/201908081639,phase=[5, 1],N=8,gpu.npz")
+print(file["phase1"])
+plt.plot(range(len(file["phase1"])),file["phase1"],"o")
+plt.plot(range(len(file["phase2"])),file["phase2"],"o")
+plt.show()
+
+
