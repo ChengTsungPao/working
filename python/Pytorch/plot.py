9727288c45f3b1fcebe4197e10d88d0233826c5e
ChengTsungPao <abaozheng6@gmail.com>
2019-08-10 18:13:16 +0800

    learning python

--- python/Pytorch/plot.py
+++ python/Pytorch/plot.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pylab as plt
 
-file = np.load("D:/program/vscode_workspace/private/data/project_train/npzfile/201908071856,phase=[5, 1],N=6,gpu.npz")
+file = np.load("D:/program/vscode_workspace/private/data/project_train/npzfile/201908091855,phase=[3, 7],N=8,gpu.npz")
 print(file["phase1"])
 print(file["phase2"])
 plt.plot(range(len(file["phase1"])),file["phase1"],"o")
