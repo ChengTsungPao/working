67e53ec37f382688b1b7379542bffba58f1bd670
ChengTsungPao <abaozheng6@gmail.com>
2021-07-07 06:19:33 +0800

    blob reducer bug

--- Javascript/agar_beta/Beta0.0/reactjs/src/Data/SendData.js
+++ Javascript/agar_beta/Beta0.0/reactjs/src/Data/SendData.js
@@ -6,17 +6,20 @@ const TEST = true;
 // 測試使用
 function PushData(newData){
     var exist = true
+    var newDataList = []  // 這邊必須建立新的List直接覆蓋data["gameData"]，不然Reducer的機制會判定狀態沒有改變而不render blob component
     for(let i = 0; i < data["gameData"].length; i++){
         if(data["gameData"][i]["_id"] === newData["_id"]){
-            data["gameData"][i]["pos"] = newData["pos"]
-            data["gameData"][i]["vel"] = newData["vel"]
-            data["gameData"][i]["rad"] = newData["rad"]
+            newDataList.push(newData)
             exist = false
+        } else {
+            newDataList.push(data["gameData"][i])
         }
     }
     if (exist) {
-        data["gameData"].push(newData)
+        newDataList.push(newData)
     }
+
+    data["gameData"] = newDataList;
     
 }
 
