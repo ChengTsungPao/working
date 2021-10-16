d65a16c900427d14ccb486a73afc016736c57889
ChengTsungPao <abaozheng6@gmail.com>
2021-10-16 10:39:41 +0800

    filter adjust

--- python/Panel Alignment System/imageFilter.py
+++ python/Panel Alignment System/imageFilter.py
@@ -1,47 +1,15 @@
 import cv2
 import matplotlib.pylab as plt
 import numpy as np
+import math
+import copy
 
-def readImage(path, filename):
-    return cv2.imread(path + filename)
-
-def cropImage(image):
-    # 裁切區域的 x 與 y 座標（左上角）
-    x = 0
-    y = 0
-
-    # 裁切區域的長度與寬度
-    w = 1080
-    h = 830
-
-    # 裁切圖片
-    return image[y : y + h , x : x + w]
-
-def contrastImage(image):
-    ######################################
-    alpha = 1.5
-    # 255 + (255 - 125) * 1.5 > 255
-    # contpre = image + (image - 0) * alpha
-    ######################################
-
-    array_alpha = np.array([1.5]) # contrast 
-    array_beta = np.array([0.0]) # brightness
-    print(image)
-    # add a beta value to every pixel 
-    image = cv2.add(image, array_beta)                    
-
-    # multiply every pixel value by alpha
-    image = cv2.multiply(image, array_alpha)
-
-    # 所有值必須介於 0~255 之間，超過255 = 255，小於 0 = 0
-    image = np.clip(image, 0, 255)
-    print("=================================")
-    print(image)
-
-    return image + (image - 125) * alpha
+import warnings
+warnings.filterwarnings("ignore")
 
 def findContour(path, filename):
-    image = readImage(path, filename)
+    originImage = readImage(path, filename)
+    image = copy.deepcopy(originImage)
 
     image = cropImage(image)
     shape = np.shape(image)
@@ -51,16 +19,109 @@ def findContour(path, filename):
     # image = contrastImage(image)
 
     threshold = cv2.threshold(image, 155, 255, cv2.THRESH_BINARY)
-    cv2.imshow("out threshold", threshold[1])
 
     canny = cv2.Canny(image, 70, 130)
-    cv2.imshow("out canny", canny)
-
     contours, hierarchy = cv2.findContours(canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    draw_img0 = cv2.drawContours(image.copy(), contours, -1, (0, 0, 255), 2)
-    cv2.imshow("contours", draw_img0)
+    contour = contours[-1]
+    draw = cv2.drawContours(image.copy(), contour, -1, (0, 0, 255), 2)
 
+    cv2.imshow("threshold", threshold[1])
+    cv2.imshow("canny", canny)
+    cv2.imshow("contour", draw)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-    # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    Gradient, data = getGradient(originImage, contour)
+    mag, angle = getAngleMag(Gradient)
+
+    plotResult(angle)
+
+    index = 348
+    drawImage(draw, data[index])
+
+def readImage(path, filename):
+    return cv2.imread(path + filename)
+
+def cropImage(image):
+    x = 0
+    y = 0
+
+    w = 1080
+    h = 830
+
+    return image[y : y + h , x : x + w]
+
+def contrastImage(image):
+    return image + (image - 125) * 1.5
+
+def getGradient(image, contour):
+    
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    Sx = [
+        [-2,  0,  2],
+        [-1,  0,  1],
+        [-2,  0,  2]
+    ]
+
+    Sy = [
+        [-2, -1, -2],
+        [ 0,  0,  0],
+        [ 2,  1,  2]
+    ]
+
+    Sx = np.array(Sx)
+    Sy = np.array(Sy)
+    shape = np.shape(image)
+
+    data = []
+    for x in contour:
+        data.append(x[0])
+    data.sort(key = lambda x: math.atan(abs(shape[0] - x[0]) / abs(shape[1] - x[1])))
+
+    Gradient = []
+    for pos in data:
+        y, x = pos[0], pos[1]
+        pixel = image[x - 1:x + 1 + 1:, y - 1:y + 1 + 1:]
+        Gx = np.sum(pixel * Sx)
+        Gy = np.sum(pixel * Sy)
+        Gradient.append((restrictRange(Gx), restrictRange(Gy)))
+
+    return Gradient, data
+
+
+def restrictRange(val):
+    return max(min(abs(val), 255), 0)
+
+
+def getAngleMag(Gradient):
+
+    mag = []
+    angle = []
+    for Gx, Gy in Gradient:
+        mag.append((Gx ** 2 + Gy ** 2) ** 0.5)
+        angle.append(math.atan(Gy / Gx) * 180 / np.pi)
+
+    return np.array(mag), np.array(angle)
+
+def plotResult(result):
+    
+    plt.subplot(211)
+    plt.title("result")
+    plt.xlabel("index of point")
+    plt.ylabel("degree")
+    plt.plot(list(range(len(result))), result)
+    
+    plt.subplot(212)
+    plt.title("dev result")
+    plt.xlabel("index of point")
+    plt.ylabel("degree")
+    plt.plot(list(range(len(result) - 1)), np.abs(result[:-1] - result[1:]))
+    plt.show()
+
+
+def drawImage(draw, point):
+    cv2.line(draw, (point[0], point[1]), (point[0], point[1]), (255, 0, 0), 5)
+    cv2.imshow("result", draw)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
--- python/Panel Alignment System/main.py
+++ python/Panel Alignment System/main.py
@@ -1,7 +1,11 @@
 from imageFilter import findContour
 
 if __name__ == "__main__":
+    # path = "./Test Image_20210913/M3mm_Deg1_Bri150/"
+    # filename = "cal_0_L.png"
+
     path = "./Test Image_20210913/M3mm_Deg1_Bri150/"
-    filename = "cal_0_L.png"
+    filename = "cal_10_L.png"
+    
     findContour(path, filename)
     
