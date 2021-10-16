e1f8798fbe066c9692eb56cdb5add7da6054ce78
chengtsungpao <abaozheng6@gmail.com>
2021-10-16 18:13:06 +0800

    auto detect

--- python/Panel Alignment System/imageFilter.py
+++ python/Panel Alignment System/imageFilter.py
@@ -7,7 +7,22 @@ import copy
 import warnings
 warnings.filterwarnings("ignore")
 
-def findContour(path, filename):
+def getParameter(light):
+    
+    table = {
+        "light": "Binary",
+            30 :  35 ,
+            50 :  80 ,
+           100 : 110 ,
+           150 : 155 ,
+           200 : 200 ,
+           255 : 245
+    }
+
+    return table[light] 
+
+
+def findContour(path, filename, light, imageType = "L"):
     originImage = readImage(path, filename)
     image = copy.deepcopy(originImage)
 
@@ -18,26 +33,27 @@ def findContour(path, filename):
     image = cv2.medianBlur(image, 3)
     # image = contrastImage(image)
 
-    threshold = cv2.threshold(image, 155, 255, cv2.THRESH_BINARY)
+    threshold = cv2.threshold(image, getParameter(light), 255, cv2.THRESH_BINARY)
 
-    canny = cv2.Canny(image, 70, 130)
+    canny = cv2.Canny(threshold[1], 70, 130)
     contours, hierarchy = cv2.findContours(canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     contour = contours[-1]
     draw = cv2.drawContours(image.copy(), contour, -1, (0, 0, 255), 2)
 
-    cv2.imshow("threshold", threshold[1])
-    cv2.imshow("canny", canny)
-    cv2.imshow("contour", draw)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # cv2.imshow("threshold", threshold[1])
+    # cv2.imshow("canny", canny)
+    # cv2.imshow("contour", draw)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
 
-    Gradient, data = getGradient(originImage, contour)
+    Gradient, data = getGradient(originImage, contour, imageType)
     mag, angle = getAngleMag(Gradient)
 
-    plotResult(angle)
+    plotResult(path, filename, angle)
 
-    index = 348
-    drawImage(draw, data[index])
+    # index = 288
+    index = int(input("index = "))
+    drawImage(path, filename, draw, data[index])
 
 def readImage(path, filename):
     return cv2.imread(path + filename)
@@ -54,7 +70,7 @@ def cropImage(image):
 def contrastImage(image):
     return image + (image - 125) * 1.5
 
-def getGradient(image, contour):
+def getGradient(image, contour, imageType):
     
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
@@ -77,8 +93,12 @@ def getGradient(image, contour):
     data = []
     for x in contour:
         data.append(x[0])
-    data.sort(key = lambda x: math.atan(abs(shape[0] - x[0]) / abs(shape[1] - x[1])))
 
+    if imageType == "L":
+        data.sort(key = lambda x: math.atan(abs(shape[0] - x[1]) / abs(shape[1] - x[0])))
+    else:
+        data.sort(key = lambda x: math.atan(abs(0 - x[1]) / abs(shape[1] - x[0])))
+    
     Gradient = []
     for pos in data:
         y, x = pos[0], pos[1]
@@ -104,7 +124,7 @@ def getAngleMag(Gradient):
 
     return np.array(mag), np.array(angle)
 
-def plotResult(result):
+def plotResult(path, filename, result):
     
     plt.subplot(211)
     plt.title("result")
@@ -117,11 +137,13 @@ def plotResult(result):
     plt.xlabel("index of point")
     plt.ylabel("degree")
     plt.plot(list(range(len(result) - 1)), np.abs(result[:-1] - result[1:]))
+    plt.savefig(path + filename.split(".png")[0] + "_angle.png", dpi = 300)
     plt.show()
 
 
-def drawImage(draw, point):
+def drawImage(path, filename, draw, point):
     cv2.line(draw, (point[0], point[1]), (point[0], point[1]), (255, 0, 0), 5)
-    cv2.imshow("result", draw)
+    # cv2.imshow("result", draw)
+    cv2.imwrite(path + filename.split(".png")[0] + "_result.png", draw)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
--- python/Panel Alignment System/main.py
+++ python/Panel Alignment System/main.py
@@ -1,11 +1,20 @@
 from imageFilter import findContour
+from glob import glob
+import os
 
 if __name__ == "__main__":
-    # path = "./Test Image_20210913/M3mm_Deg1_Bri150/"
-    # filename = "cal_0_L.png"
-
-    path = "./Test Image_20210913/M3mm_Deg1_Bri150/"
-    filename = "cal_10_L.png"
+    # paths = glob("./Test Image_20210913/M3mm*")
+    # paths = ["./Test Image_20210913\\M3mm_Deg2.5_Bri150"]
+    # for path in paths:
+    #     light = int(path.split("Bri")[-1])
+    #     path += "//"
+    #     files = os.listdir(path)
+    #     for filename in files:
+    #         if filename.split(".png")[0][-1] == "L":
+    #             findContour(path, filename, light, filename.split(".png")[0][-1])
     
-    findContour(path, filename)
+    path = "./Test Image_20210913/M3mm_Deg2.5_Bri200/"
+    filename = "cal_8_R.png"
+    light = 200
+    findContour(path, filename, light)
     
