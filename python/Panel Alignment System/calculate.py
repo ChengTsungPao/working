3a79cebf7786b9905089bbacc3d3879998d8b866
ChengTsungPao <abaozheng6@gmail.com>
2021-10-20 16:45:43 +0800

    UI design

--- python/Panel Alignment System/calculate.py
+++ python/Panel Alignment System/calculate.py
@@ -11,10 +11,11 @@ def getAngleMagnitude(Gradient):
     angle = []
     for Gx, Gy in Gradient:
         mag.append((Gx ** 2 + Gy ** 2) ** 0.5)
-        val = math.atan(Gy / Gx) * 180 / np.pi
-        if val < 0 and abs(val) > 45:
-            val += 180
-        angle.append(val)
+        # val = math.atan(Gy / Gx) * 180 / np.pi
+        # if val < 0 and abs(val) > 45:
+        #     val += 180
+        # angle.append(val)
+        angle.append(np.angle(complex(Gx, Gy), deg=True))
 
     return np.array(mag), np.array(angle)
 
