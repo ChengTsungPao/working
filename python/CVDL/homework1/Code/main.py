5b004e37f6a0311514c7e0a948444a04d24e38b4
chengtsungpao <abaozheng6@gmail.com>
2021-10-24 20:15:33 +0800

    Q3

--- python/CVDL/homework1/Code/main.py
+++ python/CVDL/homework1/Code/main.py
@@ -6,30 +6,6 @@ import numpy as np
 if __name__ == "__main__":
 
     # problem = 1
-    # index = 1
-
-    # path = "..//Dataset_CvDl_Hw1//Q{}_Image//".format(problem)
-    # filename = "{}.bmp".format(index)
-
-    # image = cv2.imread(path + filename)
-    # # corner_detection(image)
-
-    # intrinsicMatrix, translations, rotations = corner_detection(path, True)
-    # print("#########################################################")
-    # print("intrinsic matrix = ")
-    # print(intrinsicMatrix)
-
-    # print("#########################################################")
-    # print("extrinsic matrix = ")
-    # extrinsicMatrix = [] 
-    # for i in range(len(translations)):
-    #     rotationMatrix, _ = cv2.Rodrigues(rotations[i])
-    #     translationMatrix = translations[i]
-    #     extrinsicMatrix.append(np.concatenate((rotationMatrix, translationMatrix), axis = 1))
-    # extrinsicMatrix = np.array(extrinsicMatrix)
-    # print(extrinsicMatrix)
-
-    # problem = 1
     # path = "..//Dataset_CvDl_Hw1//Q{}_Image//".format(problem)
     # corner_detection_fcn = corner_detection(path)
     # corner_detection_fcn.find_corners(True)
