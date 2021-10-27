ed4000a9090e35cd98c7c334cf96fdca0c8c7e34
chengtsungpao <abaozheng6@gmail.com>
2021-10-28 04:57:39 +0800

    Q5_finish

--- python/CVDL/homework1/Code/Cifar10_Classifier.py
+++ python/CVDL/homework1/Code/Cifar10_Classifier.py
@@ -14,13 +14,22 @@ class cifar10_classifier():
 
     def __init__(self):
 
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  
-        self.model = VGG16().to(self.device)
-
+        # dataset
         transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
         self.trainset = torchvision.datasets.CIFAR10(root = './dataset', train = True, download = True, transform = transform)
         self.testset = torchvision.datasets.CIFAR10(root = './dataset', train = False, download = True, transform = transform)
 
+        # model
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  
+        self.model = VGG16().to(self.device)
+
+        # parameter
+        self.epoch = 20
+        self.batch_size = 512
+        self.learning_rate = 0.0001
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr = self.learning_rate)
+        
+        # target or kind
         self.targetTable = {
              0: "airplane",
              1: "automobile",
@@ -33,6 +42,8 @@ class cifar10_classifier():
              8: "ship",
              9: "truck"
         }
+
+        os.system('cls||clear')
 
 
     def plot_Cifa10_images(self):
@@ -52,22 +63,29 @@ class cifar10_classifier():
         plt.show()
 
 
+    def show_model_summary(self):
+
+        summary(self.model, (3, 32, 32))
+
+
+    def show_hyperparameters(self):
+
+        print("Batch Size {}\n".format(self.batch_size))
+        print(self.optimizer)
+
+
     def train_data(self):  
 
-        epoch = 20
-        batch_size = 512
-        learning_rate = 0.0001
-
-        train_dataloader = torch.utils.data.DataLoader(self.trainset, batch_size = batch_size, shuffle = True, num_workers = 2)
+        train_dataloader = torch.utils.data.DataLoader(self.trainset, batch_size = self.batch_size, shuffle = True, num_workers = 2)
         number_of_data = len(self.trainset)
         number_of_batch = len(train_dataloader)
 
-        optimizer = torch.optim.Adam(self.model.parameters(), lr = learning_rate)  
+        optimizer = self.optimizer
         loss_func = nn.CrossEntropyLoss().to(self.device)
 
         epoch_loss = []
         epoch_accuracy = []
-        for step in range(1, epoch + 1):
+        for step in range(1, self.epoch + 1):
 
             total_batch_loss = 0
             correct_predict = 0
@@ -103,24 +121,38 @@ class cifar10_classifier():
 
 
     def test_data(self, index = 0):
-        return
+
+        if not os.path.exists("./model/"):
+            self.train_data()
+
         model = torch.load("./model/model.pkl")
         shape = np.shape(self.testset[0][0])
+        
+        image, target = self.testset[index]
+        inputImage = torch.tensor(image.data.numpy().reshape(1, shape[0], shape[1], shape[2]))
+        output = model(inputImage.float().to(self.device))
+        output = nn.functional.softmax(output, dim = 1)
+        output = output.data.cpu().numpy()
 
-        correct_predict = 0
-        for image, target in self.testset:
-            image = torch.tensor(image.data.numpy().reshape(1, shape[0], shape[1], shape[2]))
-            target = torch.tensor(target.data.numpy().reshape(1, 1))
-            output = model(image.float().to(self.device))
-            _, predict = torch.max(nn.functional.softmax(output, dim = 1), 1)
-            correct_predict += (predict.data.cpu() == target).sum()
+        plt.figure(figsize = (16, 6), dpi = 80)
 
-        accuracy = correct_predict / len(self.testset)
+        plt.subplot(121)
+        image = (image.data.numpy().transpose((1, 2, 0)) + 1) / 2
+        plt.axis('off')
+        plt.imshow(image)
 
-        return accuracy
+        plt.subplot(122)
+        plt.title("kind of probability")
+        plt.bar(self.targetTable.values(), output[0])
+        plt.ylabel("probability")
+        plt.tight_layout()
+        plt.show()
 
 
     def plot_result(self):
+
+        if not os.path.exists("./predict/"):
+            self.train_data()
 
         loss = np.load("./predict/{}.npz".format("loss"))
         accuracy = np.load("./predict/{}.npz".format("accuracy"))
--- python/CVDL/homework1/Code/main.py
+++ python/CVDL/homework1/Code/main.py
@@ -36,8 +36,10 @@ if __name__ == "__main__":
 
     problem = 5
     cifar10_classifier_fcn = cifar10_classifier()
-    # cifar10_classifier_fcn.plot_Cifa10_images()
+    cifar10_classifier_fcn.plot_Cifa10_images()
+    cifar10_classifier_fcn.show_model_summary()
+    cifar10_classifier_fcn.show_hyperparameters()
     # cifar10_classifier_fcn.train_data()
     cifar10_classifier_fcn.test_data()
-    # cifar10_classifier_fcn.plot_result()
+    cifar10_classifier_fcn.plot_result()
 
