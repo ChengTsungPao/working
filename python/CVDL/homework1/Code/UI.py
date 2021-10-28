62fc973cf82bdf7c663f68018cf0291ad245f5ae
ChengTsungPao <abaozheng6@gmail.com>
2021-10-28 17:59:29 +0800

    CVDL UI

--- python/CVDL/homework1/Code/UI.py
+++ python/CVDL/homework1/Code/UI.py
@@ -1,5 +1,7 @@
 from Corner_Detection import corner_detection
 from Augmented_Reality import augmented_reality
+from Stereo_Disparity_Map import stereo_disparity_map
+from Scale_Invariant_Feature_Transform import scale_invariant_feature_transform
 
 from PyQt5 import QtWidgets, uic
 
@@ -17,14 +19,21 @@ class UI(QtWidgets.QMainWindow):
         self.Find_Extrinsic.clicked.connect(self.find_extrinsic)
         self.Find_Distortion.clicked.connect(self.find_distortion)
         self.Show_Result.clicked.connect(self.show_result)
+
         # problem 2 UI connect
         self.augmented_reality_fcn = None
-        # problem = 2
-        # path = "..//Dataset_CvDl_Hw1//Q{}_Image//".format(problem)
-        # word = "OPENCV"
-        # augmented_reality_fcn = augmented_reality(path)
-        # augmented_reality_fcn.draw_board(word)
-        # augmented_reality_fcn.draw_vertical(word)
+        self.Show_Words_on_Board.clicked.connect(self.draw_board)
+        self.Show_Words_Vertically.clicked.connect(self.draw_vertical)
+        
+        # problem = 3 UI connect
+        self.stereo_disparity_map_fcn = None
+        self.Stereo_Disparity_Map.clicked.connect(self.stereo_disparity_map)
+
+        # problem = 4 UI connect
+        self.scale_invariant_feature_transform_fcn = None
+        self.Keypoints.clicked.connect(self.find_keypoints)
+        self.Matched_keypoints.clicked.connect(self.matched_keypoints)
+        self.Warp_Image.clicked.connect(self.matched_images)
 
         self.show()
 
@@ -37,7 +46,17 @@ class UI(QtWidgets.QMainWindow):
     def setup_augmented_reality(self):
         problem = 2
         path = "..//Dataset_CvDl_Hw1//Q{}_Image//".format(problem)
-        augmented_reality_fcn = augmented_reality(path)
+        self.augmented_reality_fcn = augmented_reality(path)
+
+    def setup_stereo_disparity_map(self):
+        problem = 3
+        path = "..//Dataset_CvDl_Hw1//Q{}_Image//".format(problem)
+        self.stereo_disparity_map_fcn = stereo_disparity_map(path)
+
+    def setup_scale_invariant_feature_transform(self):
+        problem = 4
+        path = "..//Dataset_CvDl_Hw1//Q{}_Image//".format(problem)
+        self.scale_invariant_feature_transform_fcn = scale_invariant_feature_transform(path)
 
     # problem 1
     def find_corners(self):
@@ -68,4 +87,44 @@ class UI(QtWidgets.QMainWindow):
         if self.corner_detection_fcn == None:
             self.setup_camera_calibration()
 
-        self.corner_detection_fcn.show()        
+        self.corner_detection_fcn.show()  
+
+    # problem 2
+    def draw_board(self):
+        if self.augmented_reality_fcn == None:
+            self.setup_augmented_reality()
+
+        self.augmented_reality_fcn.draw_board(self.AR_InputBox.text())
+
+    def draw_vertical(self):
+        if self.augmented_reality_fcn == None:
+            self.setup_augmented_reality()
+
+        self.augmented_reality_fcn.draw_vertical(self.AR_InputBox.text())
+
+    # problem 3
+    def stereo_disparity_map(self):
+        if self.stereo_disparity_map_fcn == None:
+            self.setup_stereo_disparity_map()
+
+        self.stereo_disparity_map_fcn.stereo_disparity_map()
+        self.stereo_disparity_map_fcn.check_disparity_value()
+
+    # problem 4
+    def find_keypoints(self):
+        if self.scale_invariant_feature_transform_fcn == None:
+            self.setup_scale_invariant_feature_transform()
+
+        self.scale_invariant_feature_transform_fcn.find_keypoints()
+
+    def matched_keypoints(self):
+        if self.scale_invariant_feature_transform_fcn == None:
+            self.setup_scale_invariant_feature_transform()
+
+        self.scale_invariant_feature_transform_fcn.matched_keypoints()
+
+    def matched_images(self):
+        if self.scale_invariant_feature_transform_fcn == None:
+            self.setup_scale_invariant_feature_transform()
+
+        self.scale_invariant_feature_transform_fcn.matched_images()
--- python/CVDL/homework1/Code/main.py
+++ python/CVDL/homework1/Code/main.py
@@ -1,7 +1,3 @@
-from Corner_Detection import corner_detection
-from Augmented_Reality import augmented_reality
-from Stereo_Disparity_Map import stereo_disparity_map
-from Scale_Invariant_Feature_Transform import scale_invariant_feature_transform
 from Cifar10_Classifier import cifar10_classifier
 
 import sys
@@ -13,19 +9,6 @@ if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     ui = UI()
     sys.exit(app.exec_())
-
-    # problem = 3
-    # path = "..//Dataset_CvDl_Hw1//Q{}_Image//".format(problem)
-    # stereo_disparity_map_fcn = stereo_disparity_map(path)
-    # stereo_disparity_map_fcn.stereo_disparity_map()
-    # stereo_disparity_map_fcn.check_disparity_value()
-
-    # problem = 4
-    # path = "..//Dataset_CvDl_Hw1//Q{}_Image//".format(problem)
-    # scale_invariant_feature_transform_fcn = scale_invariant_feature_transform(path)
-    # scale_invariant_feature_transform_fcn.find_keypoints()
-    # scale_invariant_feature_transform_fcn.matched_keypoints()
-    # scale_invariant_feature_transform_fcn.matched_images()
 
     # problem = 5
     # cifar10_classifier_fcn = cifar10_classifier()
