ed3ff1a55d1e97c86a6abc2adb8d3d74859b20e1
ChengTsungPao <abaozheng6@gmail.com>
2021-11-08 16:45:14 +0800

    opencvdl Homework1

---
+++ python/OpenCvDl/homework1/Code/Homework1/Image_Processing.py
@@ -0,0 +1,59 @@
+import numpy as np
+import cv2
+
+
+class image_processing():
+
+    def __init__(self, path):
+        self.setPath(path)
+
+
+    def setPath(self, path):
+        self.path = path
+        self.sun_image = cv2.imread(self.path + "Sun.jpg")
+        self.dog_strong_image = cv2.imread(self.path + "Dog_Strong.jpg")
+        self.dog_weak_image = cv2.imread(self.path + "Dog_Weak.jpg")
+
+
+    def load_image(self):
+        cv2.imshow("Sun.jpg", self.sun_image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+
+    def color_separation(self):
+        B_channel, G_channel, R_channel = cv2.split(self.sun_image)
+        zeros = np.zeros(np.shape(B_channel), dtype = "uint8")
+
+        cv2.imshow("B_channel", cv2.merge([B_channel, zeros, zeros]))
+        cv2.imshow("G_channel", cv2.merge([zeros, G_channel, zeros]))
+        cv2.imshow("R_channel", cv2.merge([zeros, zeros, R_channel]))
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+
+    def color_transformation(self):
+        gray_weight = cv2.cvtColor(self.sun_image, cv2.COLOR_BGR2GRAY)
+        gray_average = np.array((self.sun_image[:, :, 0] + self.sun_image[:, :, 1] + self.sun_image[:, :, 2]) / 3, dtype = "uint8")
+
+        cv2.imshow("OpenCV function", gray_weight)
+        cv2.imshow("Average weighted", gray_average)
+
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()   
+
+
+    def combine_image(self, blend):
+        dog_strong_weight = (255 - blend) / 255
+        dog_weak_weight= blend / 255
+
+        image = cv2.addWeighted(self.dog_strong_image, dog_strong_weight, self.dog_weak_image, dog_weak_weight, 0)
+        cv2.imshow("color_transformation", image)
+
+
+    def blending(self):
+        cv2.imshow("color_transformation", self.dog_strong_image)
+        cv2.createTrackbar("blend", "color_transformation", 0, 255, self.combine_image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()   
+
--- python/OpenCvDl/homework1/Code/Homework1/main.py
+++ python/OpenCvDl/homework1/Code/Homework1/main.py
@@ -4,6 +4,32 @@ from UI import UI
 
 if __name__ == "__main__":
 
-    app = QtWidgets.QApplication(sys.argv)
-    ui = UI()
-    sys.exit(app.exec_())
+    # app = QtWidgets.QApplication(sys.argv)
+    # ui = UI()
+    # sys.exit(app.exec_())
+
+    from Image_Processing import image_processing
+    from Image_Smoothing import image_smoothing
+    from Edge_Detection import edge_detection
+
+    # path = "./Dataset_OpenCvDl_Hw1/Q1_Image/"
+    # image_processing_fcn = image_processing(path)
+    # image_processing_fcn.load_image()
+    # image_processing_fcn.color_separation()
+    # image_processing_fcn.color_transformation()
+    # image_processing_fcn.blending()
+
+
+    # path = "./Dataset_OpenCvDl_Hw1/Q2_Image/"
+    # image_smoothing_fcn = image_smoothing(path)
+    # image_smoothing_fcn.gaussian_blur()
+    # image_smoothing_fcn.bilateral_filter()
+    # image_smoothing_fcn.median_filter()
+
+
+    path = "./Dataset_OpenCvDl_Hw1/Q3_Image/"
+    edge_detection_fcn = edge_detection(path)
+    edge_detection_fcn.gaussian_blur()
+    edge_detection_fcn.sobelX()
+    edge_detection_fcn.sobelY()
+
