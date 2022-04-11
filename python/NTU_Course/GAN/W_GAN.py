ebc587cc18382c1c1c0a502f17a62541cffae679
chengtsungpao <abaozheng6@gmail.com>
2022-04-11 15:28:00 +0800

    GAN

--- python/NTU_Course/GAN/W_GAN.py
+++ python/NTU_Course/GAN/W_GAN.py
@@ -72,14 +72,14 @@ if __name__ == "__main__":
     os.makedirs("images", exist_ok=True)
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--n_epochs", type=int, default=500, help="number of epochs of training")
+    parser.add_argument("--n_epochs", type=int, default=100, help="number of epochs of training")
     parser.add_argument("--batch_size", type=int, default=1024, help="size of the batches")
-    parser.add_argument("--lr", type=float, default=0.00005, help="learning rate")
+    parser.add_argument("--lr", type=float, default=0.001, help="learning rate")
     parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
     parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
     parser.add_argument("--img_size", type=int, default=96, help="size of each image dimension")
     parser.add_argument("--channels", type=int, default=3, help="number of image channels")
-    parser.add_argument("--n_critic", type=int, default=5, help="number of training steps for discriminator per iter")
+    parser.add_argument("--n_critic", type=int, default=10, help="number of training steps for discriminator per iter")
     parser.add_argument("--clip_value", type=float, default=0.01, help="lower and upper clip value for disc. weights")
     parser.add_argument("--sample_interval", type=int, default=400, help="interval betwen image samples")
     opt = parser.parse_args()
@@ -166,8 +166,8 @@ if __name__ == "__main__":
             optimizer_D.step()
 
             # Clip weights of discriminator
-            for p in discriminator.parameters():
-                p.data.clamp_(-opt.clip_value, opt.clip_value)
+            # for p in discriminator.parameters():
+            #     p.data.clamp_(-opt.clip_value, opt.clip_value)
 
             # Train the generator every n_critic iterations
             if i % opt.n_critic == 0:
