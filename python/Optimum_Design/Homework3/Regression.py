d700e93f99139bdc06ddc44a3e2e0d43f435306a
chengtsungpao <abaozheng6@gmail.com>
2022-04-21 18:20:07 +0800

    finish v1

--- python/Optimum_Design/Homework3/Regression.py
+++ python/Optimum_Design/Homework3/Regression.py
@@ -1,5 +1,7 @@
 from GoldSearch import goldSearch
+import matplotlib.pylab as plt
 import numpy as np
+
 
 class linearRegression:
 
@@ -133,10 +135,73 @@ class polynomialRegression:
         return (np.dot(np.dot(m, x.T), y)).T
 
 
-def printPolynomial(constant, d = 5):
-    ans = str(round(constant[0], d))
-    for i in range(1, len(constant)):
-        c = constant[i]
-        symbol = "+" if c >= 0 else "-"
-        ans += " {} {} x^{}".format(symbol, round(abs(c), d), i) if i != 1 else " {} {} x".format(symbol, round(abs(c), d))
-    print("f(x) = {}".format(ans))
+class constructRegression:
+
+    def __init__(self, xData, yData):
+        self.xData = xData
+        self.yData = yData
+
+    def regressionFunction(self, position):
+        a0, a1, a2 = position
+        return sum([(y - (a2 * np.sin(x) + a1 * x + a0)) ** 2 for x, y in zip(self.xData, self.yData)])
+
+    def getGradient(self, position):
+        a0, a1, a2 = position
+        d0 = sum([2 * (y - (a2 * np.sin(10 * x) + a1 * x + a0)) * (-1)              for x, y in zip(self.xData, self.yData)])
+        d1 = sum([2 * (y - (a2 * np.sin(10 * x) + a1 * x + a0)) * (-x)              for x, y in zip(self.xData, self.yData)])
+        d2 = sum([2 * (y - (a2 * np.sin(10 * x) + a1 * x + a0)) * (-np.cos(10 * x)) for x, y in zip(self.xData, self.yData)])
+        return np.array([d0, d1, d2], float)
+
+    def fletcherReevesMethod(self, start = [0, 0, 0], iteration = 100):
+        position = np.array(start, float)
+        gradient = self.getGradient(position)
+        s = -gradient
+
+        for _ in range(iteration):
+            _lambda, _, _ = goldSearch(lambda x: self.regressionFunction(position + x * s), 0, 50)
+            newPosition = position + _lambda * s
+            newGradient = self.getGradient(newPosition)
+            beta = np.dot(newGradient, newGradient) / np.dot(gradient, gradient)
+            newS = -newGradient + beta * s
+
+            position = newPosition.copy()
+            gradient = newGradient.copy()
+            s = newS.copy()
+
+        return position
+
+
+def printFunction(coefficient, isPolynomial = True, d = 3):
+    coefficient = [round(c, d) for c in coefficient]
+
+    def printPolynomial(coefficient, d):
+        printString = str(coefficient[0])
+        for i in range(1, len(coefficient)):
+            c = coefficient[i]
+            symbol = "+" if c >= 0 else "-"
+            printString += " {} {} x^{}".format(symbol, abs(c), i) if i != 1 else " {} {} x".format(symbol, abs(c))
+        printString = "f(x) = " + printString
+        return printString
+
+    printString = printPolynomial(coefficient, d) if isPolynomial else "f(x) = {} sin(x) + {}x + {}".format(coefficient[2], coefficient[1], coefficient[0])
+    print(printString)
+    return printString
+
+
+def plotData(xData, yData, title = "", function = None):
+
+    def plotFunction():
+        if not function:
+            return
+        X = np.linspace(0, 12, 100)
+        Y = [function(x) for x in X]
+        plt.plot(X, Y)
+
+    data = np.array(sorted(zip(xData, yData)))
+    plt.figure(figsize = (10, 6))
+    plt.title(title)
+    plt.plot(data[:, 0], data[:, 1], "-o")
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plotFunction()
+    plt.show()
--- python/Optimum_Design/Homework3/main.py
+++ python/Optimum_Design/Homework3/main.py
@@ -1,22 +1,38 @@
-from Regression import linearRegression, quadraticRegression, polynomialRegression, printPolynomial
-
+from Regression import linearRegression, quadraticRegression, polynomialRegression, constructRegression, printFunction, plotData
+import numpy as np
 
 if __name__ == "__main__":
     dataX = [0.1, 0.9, 1.9, 2.3, 3, 4.1, 5.2, 5.9, 6.8, 8.1, 8.7, 9.2, 10.1, 12]
     dataY = [20, 24, 27, 29, 32, 37.3, 36.4, 32.4, 28.5, 30, 38, 43, 40, 32]
 
+    ############################################ Problem 2 #############################################
+    print("\nProblem 2")
     linearRegression_func = linearRegression(dataX, dataY)
-    result = linearRegression_func.fletcherReevesMethod([0, 0])
-    printPolynomial(result)
-    result = linearRegression_func.newtonMethod([0, 0])
-    printPolynomial(result)
+    coefficient = linearRegression_func.fletcherReevesMethod([0, 0])
+    printString = printFunction(coefficient)
+    plotData(dataX, dataY, printString, lambda x: sum([a * x ** k for k, a in enumerate(coefficient)]))
+    coefficient = linearRegression_func.newtonMethod([0, 0])
+    printString = printFunction(coefficient)
+    plotData(dataX, dataY, printString, lambda x: sum([a * x ** k for k, a in enumerate(coefficient)]))
 
+    ############################################ Problem 3 #############################################
+    print("\nProblem 3")
     quadraticRegression_func = quadraticRegression(dataX, dataY)
-    result = quadraticRegression_func.fletcherReevesMethod([0, 0, 0])
-    printPolynomial(result)
-    result = quadraticRegression_func.newtonMethod([0, 0, 0])
-    printPolynomial(result)
-
+    coefficient = quadraticRegression_func.fletcherReevesMethod([0, 0, 0])
+    printString = printFunction(coefficient)
+    plotData(dataX, dataY, printString, lambda x: sum([a * x ** k for k, a in enumerate(coefficient)]))
+    coefficient = quadraticRegression_func.newtonMethod([0, 0, 0])
+    printString = printFunction(coefficient)
+    plotData(dataX, dataY, printString, lambda x: sum([a * x ** k for k, a in enumerate(coefficient)]))
+    
     polynomialRegression_func = polynomialRegression(dataX, dataY)
-    result = polynomialRegression_func.polynomialRegressionMethod(6)
-    printPolynomial(result)
+    coefficient = polynomialRegression_func.polynomialRegressionMethod(5)
+    printString = printFunction(coefficient)
+    plotData(dataX, dataY, printString, lambda x: sum([a * x ** k for k, a in enumerate(coefficient)]))
+
+    ############################################ Problem 4 #############################################
+    print("\nProblem 4")
+    constructRegression_func = constructRegression(dataX, dataY)
+    coefficient = constructRegression_func.fletcherReevesMethod()
+    printString = printFunction(coefficient, False)
+    plotData(dataX, dataY, printString, lambda x: coefficient[2] * np.sin(x) + coefficient[1] * x + coefficient[0])
