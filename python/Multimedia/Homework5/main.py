42e67b12e2658f8af56cba5200d4a9735009da11
chengtsungpao <abaozheng6@gmail.com>
2022-05-06 18:14:08 +0800

    Multimedia_HW5

--- python/Multimedia/Homework5/main.py
+++ python/Multimedia/Homework5/main.py
@@ -5,18 +5,14 @@ if __name__ == "__main__":
     kind = ["blues", "classical", "country", "disco", "hiphop", "jazz", "metal", "pop", "reggae", "rock"]
 
     path = "./dataset"
-    dataset = music_genre_dataset(path)
+    # dataset = music_genre_dataset(path)
     # dataset.plotOriginData(kind[1], 0)
     # dataset.plotFFTData(kind[1], 0)
     # dataset.plotSpectogramData(kind[1], 0)
 
     music_genre_train_func = music_genre_train(path)
-    # music_genre_train_func.train(2)
-
+    music_genre_train_func.train(2)
 
     filename = "2022_0506_0545_Spectrogram"
     music_genre_train_func.predict(filename)
     music_genre_train_func.plotResult(filename)
-
-
-
