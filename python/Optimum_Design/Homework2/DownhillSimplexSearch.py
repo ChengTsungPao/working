ee0538cfa8c7f9386ac1163d0e230974e26c6ceb
ChengTsungPao <abaozheng6@gmail.com>
2022-05-09 19:01:34 +0800

    DownhillSimplexSearch update

--- python/Optimum_Design/Homework2/DownhillSimplexSearch.py
+++ python/Optimum_Design/Homework2/DownhillSimplexSearch.py
@@ -1,4 +1,4 @@
-from turtle import pos
+from turtle import pos, position
 import numpy as np
 
 def downhillSimplexSearch(f, start, iteration = 100, parameter = [1, 0.5, 2]):
@@ -57,4 +57,77 @@ def downhillSimplexSearch(f, start, iteration = 100, parameter = [1, 0.5, 2]):
     return np.array(minPositions), np.array(values)
 
 
+def downhillSimplexSearchTeacher(f, start, iteration = 100, parameter = [1, 0.5, 2]):
+    
+    values = []
+    alpha, beta, gamma = parameter
+    positions = np.array(sorted(start, key = lambda position: f(position)), float)
+    minPositions = []
 
+    for _ in range(iteration):
+
+        print("============================= iteration = {} =============================".format(iteration))
+
+        # Order
+        positions = np.array(sorted(positions, key = lambda position: f(position)), float)
+
+        # Step 1
+        xAvg = np.average(positions[:-1], axis = 0)
+        xMin = positions[0]
+        xMax = positions[-1]
+        xSecondMax = positions[-2]
+        minPositions.append(np.average(positions, axis = 0))
+        values.append(f(xMin))
+        print("Step 1 => xMin = {}, xMax = {}, xSecondMax = {}, xAvg = {} (No xMax)".format(xMin, xMax, xSecondMax, xAvg))
+
+        # Step 2
+        xr = xAvg + alpha * (xAvg - xMax)
+        if f(xMin) <= f(xr): 
+            print("\nBecause: f(xMin) = {} <= f(xr) = {}".format(f(xMin), f(xr)))
+            print("Step 2 => xr = {}".format(xr))
+
+        if f(xMin) > f(xr):
+            xe = xAvg + gamma * (xr - xAvg)
+            print("\nBecause: f(xMin) = {} > f(xr) = {}".format(f(xMin), f(xr)))
+            print("Step 2 => xr = {}, xe = {}".format(xr, xe))
+
+            # Step 3
+            if f(xr) > f(xe):
+                print("\nBecause: f(xr) = {} > f(xe) = {}".format(f(xr), f(xe)))
+                print("Step 3 => xMax = xe = {}".format(xe))
+                positions[-1] = xe
+            else:
+                print("\nBecause: f(xr) = {} <= f(xe) = {}".format(f(xr), f(xe)))
+                print("Step 3 => xMax = xr = {}".format(xr))
+                positions[-1] = xr
+            continue
+
+        # Step 4
+        if f(xSecondMax) >= f(xr):
+            print("\nBecause: f(xSecondMax) = {} >= f(xr) = {}".format(f(xSecondMax), f(xr)))
+            print("Step 4 => xMax = xr = {}".format(xr))
+            positions[-1] = xr
+            continue
+        
+        # Step 5
+        xp = min(xr, xMax, key = lambda x: f(x))
+        xc = xAvg + beta * (xp - xAvg)
+        if f(xc) > f(xp):
+            positions = [position + (xMin - position) / 2 for position in positions]
+            print("\nBecause: f(xc) = {} > f(xp) = {}".format(f(xc), f(xp)))
+            print("Step 5 => position = position + (xMin - position) / 2 = {}".format(positions))
+        else:
+            print("\nBecause: f(xc) = {} > f(xr) = {}".format(f(xc), f(xr)))
+            print("Step 5 => xMax = xc = {}".format(xc))
+            positions[-1] = xc
+
+    return np.array(minPositions), np.array(values)
+
+
+def f(position):
+    x = position
+    return x / (1 + x ** 2)
+
+if __name__ == "__main__":
+    positions1, values1 = downhillSimplexSearchTeacher(f, [-2, 0, -2], 3)
+    print("\nlocal min value = {}".format(values1[-1]))
