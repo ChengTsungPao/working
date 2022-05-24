6d1279e60d51aa413e506f9cedec2a25b24e2c37
ChengTsungPao <abaozheng6@gmail.com>
2022-05-24 16:16:52 +0800

    finish v2

--- python/Optimum_Design/Homework4/DownhillSimplexSearch.py
+++ python/Optimum_Design/Homework4/DownhillSimplexSearch.py
@@ -1,109 +1,59 @@
 import numpy as np
 
-def downhillSimplexSearch(f, start, iteration = 100, parameter = [1, 0.5, 2]):
-
-    values = []
-    alpha, beta, gamma = parameter
-    positions = np.array(sorted(start, key = lambda position: f(position)), float)
-    minPositions = []
-
-    for _ in range(iteration):
-
-        # Order
-        positions = np.array(sorted(positions, key = lambda position: f(position)), float)
-
-        # Calculate x0
-        x0 = np.average(positions[:-1], axis = 0)
-        x1 = positions[0]
-        xn = positions[-2]
-        xn_1 = positions[-1]
-        minPositions.append(np.average(positions, axis = 0))
-        values.append(f(x1))
-
-        # Reflection
-        xr = x0 + alpha * (x0 - xn_1)
-        if f(x0) <= f(xr) < f(xn):
-            positions[-1] = xr
-            continue
-
-        # Expansion
-        if f(xr) < f(x1):
-            xe = x0 + gamma * (xr - x0)
-            if f(xe) < f(xr):
-                positions[-1] = xe
-            else:
-                positions[-1] = xr
-            continue
-
-        # Contraction
-        if f(xr) >= f(xn):
-            if f(xr) < f(xn_1):
-                xc = x0 + beta * (xr - x0)
-                if f(xc) < f(xr):
-                    positions[-1] = xc
-                    continue
-            else:
-                xc = x0 + beta * (xn_1 - x0)                
-                if f(xc) < f(xn_1):
-                    positions[-1] = xc
-                    continue    
-
-        # Shrink
-        for i in range(1, len(positions)):
-            xi = positions[i]
-            positions[i] = x1 + beta * (xi - x1)    
-
-    return np.array(minPositions), np.array(values)
-
-
-def downhillSimplexSearchTeacher(f, start, iteration = 100, parameter = [1, 0.5, 2]):
+def downhillSimplexSearch(f, start, rk = 0, iteration = 100, parameter = [1, 0.5, 2]):
     
     values = []
     alpha, beta, gamma = parameter
-    positions = np.array(sorted(start, key = lambda position: f(position)), float)
+    positions = np.array(sorted(start, key = lambda position: f(position, rk)), float)
     minPositions = []
 
-    for iter in range(iteration):
+    while rk < 10 ** 10:
 
-        # Order
-        positions = np.array(sorted(positions, key = lambda position: f(position)), float)
+        for _ in range(iteration):           
 
-        # Step 1
-        xAvg = np.average(positions[:-1], axis = 0)
-        xMin = positions[0]
-        xMax = positions[-1]
-        xSecondMax = positions[-2]
-        minPositions.append(np.average(positions, axis = 0))
-        values.append(f(xMin))
+            # Order
+            positions = np.array(sorted(positions, key = lambda position: f(position, rk)), float)
 
-        # Step 2
-        xr = xAvg + alpha * (xAvg - xMax)
-        if f(xMin) > f(xr):
-            xe = xAvg + gamma * (xr - xAvg)
+            # Step 1
+            xAvg = np.average(positions[:-1], axis = 0)
+            xMin = positions[0]
+            xMax = positions[-1]
+            xSecondMax = positions[-2]
+            minPositions.append(np.average(positions, axis = 0))
+            values.append(f(xMin, rk))
 
-            # Step 3
-            if f(xr) > f(xe):
-                positions[-1] = xe
-            else:
+            # Step 2
+            xr = xAvg + alpha * (xAvg - xMax)
+            if f(xMin, rk) > f(xr, rk):
+                xe = xAvg + gamma * (xr - xAvg)
+
+                # Step 3
+                if f(xr, rk) > f(xe, rk):
+                    positions[-1] = xe
+                else:
+                    positions[-1] = xr
+                continue
+
+            # Step 4
+            if f(xSecondMax, rk) >= f(xr, rk):
                 positions[-1] = xr
-            continue
+                continue
+            
+            # Step 5
+            xp = min(xr, xMax, key = lambda x: f(x, rk))
+            xc = xAvg + beta * (xp - xAvg)
+            if f(xc, rk) > f(xp, rk):
+                positions = [position + (xMin - position) / 2 for position in positions]
+            else:
+                positions[-1] = xc
 
-        # Step 4
-        if f(xSecondMax) >= f(xr):
-            positions[-1] = xr
-            continue
-        
-        # Step 5
-        xp = min(xr, xMax, key = lambda x: f(x))
-        xc = xAvg + beta * (xp - xAvg)
-        if f(xc) > f(xp):
-            positions = [position + (xMin - position) / 2 for position in positions]
-        else:
-            positions[-1] = xc
+        rk *= 10
+        if rk == 0:
+            break
 
-    positions = np.array(sorted(positions, key = lambda position: f(position)), float)
+    positions = np.array(sorted(positions, key = lambda position: f(position, rk)), float)
     xMin = positions[0]
     minPositions.append(np.average(positions, axis = 0))
-    values.append(f(xMin))
+    values.append(f(xMin, rk))
 
     return np.array(minPositions), np.array(values)
--- python/Optimum_Design/Homework4/main.py
+++ python/Optimum_Design/Homework4/main.py
@@ -1,4 +1,5 @@
-from DownhillSimplexSearch import downhillSimplexSearch, downhillSimplexSearchTeacher
+from tracemalloc import start
+from DownhillSimplexSearch import downhillSimplexSearch
 
 import numpy as np
 import warnings
@@ -11,7 +12,7 @@ def getFunction1Pos(position):
     h = 20 / (np.pi * r * r)
     return [r, h]
 
-def f1(position):
+def f1(position, rk):
     r, h = getFunction1Pos(position)
     return 2 * np.pi * r * r + 2 * np.pi * r * h
 
@@ -27,24 +28,52 @@ def getFunction2Pos(position):
     F9 = (d[5]["h"] * F5 - d[7]["h"] * F7 + d[8] * F8 - M[3]) / d[9]
     return [F1, F2, F3, F4, F5, F6, F7, F8, F9]
 
-def f2(position):
+def getFunction2Val(position):
     A = [None, 11.5, 92.5, 44.3, 98.1, 20.1, 6.1, 45.5, 31.0, 44.3]
     F = [None] + getFunction2Pos(position)
     return sum([(F[i] / A[i]) ** 2 for i in range(1, 9 + 1)])
 
+def f2(position, rk):
+    A = [None, 11.5, 92.5, 44.3, 98.1, 20.1, 6.1, 45.5, 31.0, 44.3]
+    F = [None] + getFunction2Pos(position)
+    return sum([(F[i] / A[i]) ** 2 + rk * max(-F[i], 0) ** 2 for i in range(1, 9 + 1)])
 
 if __name__ == "__main__":
 
     ########################################## Problem1 ##########################################
 
     print("\nProblem1")
-    positions1, values1 = downhillSimplexSearchTeacher(f1, [1, 3, 2])
+    positions1, values1 = downhillSimplexSearch(f1, [1, 3, 2])
     print("  [r, h] = {}".format(getFunction1Pos(positions1[-1])))
     print("  A = {}".format(values1[-1]))
 
     ########################################## Problem2 ##########################################
-    
+
+    startF = [[13, 19,  2,  1, 10, 13],
+              [ 8, 15, 10, 15, 13, 13],
+              [18,  8,  1,  8, 19,  7],
+              [16, 15, 19, 16,  4, 17],
+              [10, 20, 13,  9, 15, 20],
+              [17, 11,  1,  9, 12, 20]]
+
     print("\nProblem2")
-    positions2, values2 = downhillSimplexSearchTeacher(f2, [[1] * 6, [3] * 6, [2] * 6])
+    startF = list(startF) + [np.average(startF, axis = 0)]
+    positions2, values2 = downhillSimplexSearch(f2, startF, 1)
     print("  F = {}".format(getFunction2Pos(positions2[-1])))
-    print("  Z = {}".format(values2[-1]))
+    print("  Z = {}".format(getFunction2Val(positions2[-1])))
+
+    ########################################## Problem2 (Best Answer) ##########################################
+
+    print("\nProblem2 (Best Answer)")
+    value = float("inf")
+    positions = None
+    for _ in range(10):
+        startF = np.random.randint(1, 100, size=(6, 6))
+        startF = list(startF) + [np.average(startF, axis = 0)]
+        positions2, values2 = downhillSimplexSearch(f2, startF, 1)
+        if value > getFunction2Val(positions2[-1]) and all([pos >= 0 for pos in positions2[-1]]):
+            value = getFunction2Val(positions2[-1])
+            positions = positions2[-1]
+
+    print("  F = {}".format(getFunction2Pos(positions)))
+    print("  Z = {}".format(getFunction2Val(positions)))
