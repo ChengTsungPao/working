c7361736f344256a61b18653f9a3d19728945e4c
chengtsungpao <abaozheng6@gmail.com>
2022-08-04 23:53:10 +0800

    predict result (offset need to check)

--- python/NCKU_LAB_Paper/3DUnet/LungNodule/model/head/CornerNetHead/utils/prediction.py
+++ python/NCKU_LAB_Paper/3DUnet/LungNodule/model/head/CornerNetHead/utils/prediction.py
@@ -1,18 +1,51 @@
 import tensorflow as tf
+import numpy as np
 
-def modelPrediction2(TLF, BRB, threshold = 0.8):    
+def predictionCornerPoint(tlf_heatMap, brb_heatMap, threshold):  
+    print("Predict Corner Point...")  
 
-    TLF_heatMap, TLF_group, TLF_regression = TLF
-    BRB_heatMap, BRB_group, BRB_regression = BRB
-
-    tlf_points = []
-    for tlf in TLF_heatMap:
-        tlf = tf.exp(tlf) / tf.reduce_sum(tf.exp(tlf), axis = tlf.shape, keepdims=True)
-        tlf_points.append(tf.where(tlf > threshold).numpy())
-    
-    brb_points = []
-    for brb in BRB_heatMap:
-        brb = tf.exp(brb) / tf.reduce_sum(tf.exp(brb), axis = brb.shape, keepdims=True)
-        brb_points.append(tf.where(brb > threshold).numpy())
+    tlf_points = tf.where(tlf_heatMap > threshold[0]).numpy()   
+    brb_points = tf.where(brb_heatMap > threshold[1]).numpy()
 
     return tlf_points, brb_points
+
+
+def predictionCornerGroup(tlf_points, brb_points, tlf_group, brb_group):
+    print("Predict Corner Group...")  
+    
+    bboxes = []
+    for tlf_point in tlf_points:
+        _, x1, y1, z1, _ = tlf_point
+        _, x2, y2, z2, _ = min(brb_points, key = lambda pos: abs(brb_group[0][pos[1]][pos[2]][pos[3]][0] - tlf_group[0][x1][y1][z1][0]))
+        bboxes.append((x1, y1, z1, x2, y2, z2))
+
+    return bboxes
+
+
+def predictionBBoxOffset(bboxes, tlf_regression, brb_regression, scale):
+    print("Predict Corner offset...")  
+
+    def offset(x, y, z, regression):
+        ox, oy, oz = regression[0][x][y][z]
+        return x * scale - ox, y * scale - oy, z * scale - oz
+    
+    offsetBBoxes = []
+    for bbox in bboxes:
+        x1, y1, z1, x2, y2, z2 = bbox
+        x1, y1, z1 = offset(x1, y1, z1, tlf_regression)
+        x2, y2, z2 = offset(x2, y2, z2, brb_regression)
+        offsetBBoxes.append((int(x1.numpy()), int(y1.numpy()), int(z1.numpy()), int(x2.numpy()), int(y2.numpy()), int(z2.numpy())))
+
+    return offsetBBoxes
+
+
+def predictionModel2(predicts, scale, threshold = 0.8):
+
+    tlf_heatMap, tlf_group, tlf_regression = predicts[0: 3]
+    brb_heatMap, brb_group, brb_regression = predicts[3: 6]
+
+    tlf_points, brb_points = predictionCornerPoint(tlf_heatMap, brb_heatMap, threshold)
+    bboxes                 = predictionCornerGroup(tlf_points, brb_points, tlf_group, brb_group)
+    offsetBBoxes           = predictionBBoxOffset(bboxes, tlf_regression, brb_regression, scale)
+
+    return offsetBBoxes
---
+++ python/NCKU_LAB_Paper/3DUnet/LungNodule/test_model.py
@@ -0,0 +1,47 @@
+from model.create_model import getModel2
+from model.head.CornerNetHead.utils.prediction import predictionModel2
+import tensorflow as tf
+import numpy as np
+from skimage import transform
+
+IMAGE_SPATIAL_DIMS = [(128, 128, 128), (128, 128, 128)]
+IMAGE_NUM_CHANNELS = [1, 1]
+RESIZE_SHAPE = (128, 128, 128)
+
+
+def predict_model(model):
+    threshold = (0.5, 0.5)
+    
+    scale = IMAGE_SPATIAL_DIMS[0][0] // IMAGE_SPATIAL_DIMS[0][1]
+
+    image = np.load("./dataset/0005.npz")["image"]
+    image = image.transpose((0, 2, 3, 1))
+    image = transform.resize(image, (1,) + RESIZE_SHAPE)
+    image = np.expand_dims(image, -1)
+    image = image / 255.
+
+    outputs = model(image)
+    print("\n======================== predict ========================")
+    print("TLF => heatMap    : {} (max: {:.7f}, min: {:.7f})".format(outputs[0].shape, np.max(outputs[0]), np.min(outputs[0])))
+    print("TLF => group      : {}".format(outputs[1].shape))
+    print("TLF => regression : {}".format(outputs[2].shape))
+    print("BRB => heatMap    : {} (max: {:.7f}, min: {:.7f})".format(outputs[3].shape, np.max(outputs[3]), np.min(outputs[3])))
+    print("BRB => group      : {}".format(outputs[4].shape))
+    print("BRB => regression : {}".format(outputs[5].shape))
+
+    print("\n======================== result ========================")
+    result = predictionModel2(outputs, scale, threshold)
+    print("result => {}".format(result))
+
+
+def test():
+    # model_path = "./result/2022_0802_2335/model_epoch000"
+    model_path = "./result/2022_0804_1703/model_epoch004"
+    model = getModel2(IMAGE_SPATIAL_DIMS, IMAGE_NUM_CHANNELS, model_path)
+    predict_model(model)
+
+if __name__ == "__main__":
+    physical_devices = tf.config.experimental.list_physical_devices("GPU")
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+    test()
