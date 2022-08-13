a85e3a4cdd48146e1c6cc62451a9ae99d73db5cf
chengtsungpao <abaozheng6@gmail.com>
2022-08-13 19:53:33 +0800

    inference

--- python/NCKU_LAB_Paper/3DUnet/LungNodule/test_3dunet_model.py
+++ python/NCKU_LAB_Paper/3DUnet/LungNodule/test_3dunet_model.py
@@ -1,3 +1,4 @@
+from operator import mod
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
@@ -13,12 +14,13 @@ import matplotlib.pylab as plt
 
 TEST = True
 
+mode = "test"
 THRESHOLD = 0.5
 
 IMAGE_SPATIAL_DIMS = (512, 512, 32)
 IMAGE_NUM_CHANNELS = 1
 SAVE_PATH = "./result/2022_0812_1253_3dunet/"
-DATA_PATH = "E:\\dataset\\NCKU\\Lung\\train\\"
+DATA_PATH = "E:\\dataset\\NCKU\\Lung\\{}\\".format(mode)
 
 
 def predict_model(model_path):
@@ -34,8 +36,9 @@ def predict_model(model_path):
     for datas, sliceIDList, patientID in getInferenceGenerator(DATA_PATH):
 
         startSliceID, endSliceID = str(sliceIDList[0]).zfill(3), str(sliceIDList[-1]).zfill(3)
-        if int(patientID) < 258:
-            continue
+        # if int(patientID) < 258:
+        #     print("patientID = {} skip !!!".format(patientID))
+        #     continue
 
         outputs = model(datas)
 
@@ -50,7 +53,7 @@ def predict_model(model_path):
         count = collections.Counter(binaryMaps.flatten())
         print("status = {}".format(str(count)))
         
-        save_path = os.path.join(model_path.split(".hdf5")[0], "patientID={},sliceID={}~{}".format(patientID, startSliceID, endSliceID))
+        save_path = os.path.join(model_path.split(".hdf5")[0], mode, "patientID={},sliceID={}~{}".format(patientID, startSliceID, endSliceID))
 
         for heapMap, binaryMap, sliceID in zip(heapMaps, binaryMaps, sliceIDList):
             heapMap_name = "heapMap,patientID={},sliceID={}.jpg".format(patientID, str(sliceID).zfill(3))
@@ -59,15 +62,15 @@ def predict_model(model_path):
                 os.makedirs(save_path)
 
             heapMap_save_path = os.path.join(save_path, heapMap_name)
-            if not os.path.exists(heapMap_save_path):
-                plt.clf()
-                plt.imshow(heapMap, cmap = "hot")
-                plt.colorbar()
-                plt.savefig(heapMap_save_path)
+            # if not os.path.exists(heapMap_save_path):
+            plt.clf()
+            plt.imshow(heapMap, cmap = "hot", vmin = 0, vmax = 1)
+            plt.colorbar()
+            plt.savefig(heapMap_save_path)
 
             binaryMap_save_path = os.path.join(save_path, binaryMap_name)
-            if not os.path.exists(binaryMap_save_path):
-                cv2.imwrite(binaryMap_save_path, binaryMap)
+            # if not os.path.exists(binaryMap_save_path):
+            cv2.imwrite(binaryMap_save_path, binaryMap)
 
 def test():
     model_path = os.path.join(SAVE_PATH, "model-15.hdf5")
